fbeabe4e6482a1a275383f43c1315d66c9fd02f8
Mohemed-khan <108809525+Mohemed-khan@users.noreply.github.com>
2023-06-15 08:03:42 +0530

    Update ex3.c

--- module1/day2/level1/ex3.c
+++ module1/day2/level1/ex3.c
@@ -1,3 +1,6 @@
+/* Write a program to sum every alternate elements of a given array starting for element 0
+For example, let's say we have a[5] = {10, 20, 30, 40, 50}, the result should be 10 + 30 + 50 = 90 */
+
 #include <stdio.h>
 
 int sumAlternateElements(int arr[], int size) {
